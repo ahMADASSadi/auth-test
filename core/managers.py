c0dd76d63b9ddaad6479fc2d8aa9f216bb6b58ce
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-04-16 20:37:00 +0330

    ⚡️ Configure app
    managers and app scripts created

---
+++ core/managers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('Phone number must be provided')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(phone_number, password, **extra_fields)
